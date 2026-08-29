6948c4a87ba32a852455de745f0cdae2e27e505a
Hoàng Cao Minh <hoangcaominh1812@gmail.com>
2026-08-29 15:10:06 +1000

    Update schema.sql - Added users.location - Added posts.title

--- database/schema.sql
+++ database/schema.sql
@@ -1,18 +1,20 @@
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
 	id integer PRIMARY KEY AUTOINCREMENT,
 	username text UNIQUE NOT NULL,
 	email text UNIQUE NOT NULL,
-	password text NOT NULL
+	password text NOT NULL,
+	location text NOT NULL
 );
 
-CREATE TABLE post_category (
+CREATE TABLE IF NOT EXISTS post_category (
 	id integer PRIMARY KEY,
 	typename text NOT NULL
 );
 
-CREATE TABLE posts (
+CREATE TABLE IF NOT EXISTS posts (
 	id integer PRIMARY KEY AUTOINCREMENT,
 	owner integer NOT NULL REFERENCES users(id),
+	title text NOT NULL,
 	description text NOT NULL,
 	category_id integer NOT NULL REFERENCES post_category(id),
 	is_open integer NOT NULL DEFAULT 1 CHECK (is_open IN (0, 1))
