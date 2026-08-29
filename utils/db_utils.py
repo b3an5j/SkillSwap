1431740268b45267a76d0b480ed5d90db6fb141e
Hoàng Cao Minh <hoangcaominh1812@gmail.com>
2026-08-29 16:18:39 +1000

    Add post insert, update and delete

--- utils/db_utils.py
+++ utils/db_utils.py
@@ -10,13 +10,99 @@ def get_db():
     return conn
 
 def init_db():
-    conn = get_db()
-    cursor = conn.cursor()
+    conn = None
+    try:
+        conn = get_db()
+        cursor = conn.cursor()
 
-    # Read schema.sql and execute it
-    with open(SCHEMA_PATH, "r") as f:
-        schema = f.read()
-        cursor.executescript(schema)
+        # Read schema.sql and execute it
+        with open(SCHEMA_PATH, "r") as f:
+            schema = f.read()
+            cursor.executescript(schema)
 
-    conn.commit()
-    conn.close()
+        conn.commit()
+        conn.close()
+
+        return True
+    except Exception:
+        if conn:
+            conn.rollback()
+            conn.close()
+        return False
+
+def insert_post(data):
+    conn = None
+    try:
+        conn = get_db()
+        cur = conn.cursor()
+
+        cur.execute("""
+            INSERT INTO posts (owner, title, description, category_id, is_open)
+            VALUES (?,?,?,?,?)
+        """, (data.user_id, data.title, data.description, data.category_id, data.is_open))
+        conn.commit()
+
+        # Print something
+
+        cur.close()
+        conn.close()
+
+        return True
+    except Exception:
+        if conn:
+            conn.rollback()
+            conn.close()
+        return False
+
+def update_post(data):
+    conn = None
+    try:
+        conn = get_db()
+        cur = conn.cursor()
+
+        cur.execute("""
+            UPDATE posts
+            SET
+                title = ?,
+                description = ?,
+                category_id = ?,
+                is_open = ?
+            WHERE owner = ? AND id = ?
+        """, (data.title, data.description, data.category_id, data.is_open, data.user_id, data.post_id))
+        conn.commit()
+
+        # Print something
+
+        cur.close()
+        conn.close()
+
+        return True
+    except Exception:
+        if conn:
+            conn.rollback()
+            conn.close()
+        return False
+
+def delete_post(data):
+    conn = None
+    try:
+        conn = get_db()
+        cur = conn.cursor()
+
+        cur.execute("""
+            DELETE FROM posts
+            WHERE owner = ? AND id = ?
+        """, (data.user_id, data.post_id))
+        conn.commit()
+
+        # Print something
+
+        cur.close()
+        conn.close()
+
+        return True
+    except Exception:
+        if conn:
+            conn.rollback()
+            conn.close()
+        return False
